8503803f1097dd018deb29689a1da5809907dc1c
WesleyJw <josewesley.silva@ufrpe.br>
2021-11-02 16:04:40 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/WesleyJw" target="_blank" rel="noreferrer">Dr. Wesley Lima</a></p>
+                <p className="text-light"><small><strong>Doutor em Biometria e Estatística Aplicada</strong><br/>
+                    Para conhecer meu portfólio acesse: <a href="https://wesleyjw.github.io/" target="_blank" rel="noreferrer">wesleyjw.github.io</a></small></p>
+            </div>
+        </footer>
+  );
+}
+
+export default Footer;
